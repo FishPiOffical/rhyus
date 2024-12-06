35b482d013ddc457c4012112be083f2f9b78ceb3
陈辉 <1101635162@qq.com>
2024-12-06 21:52:41 +0800

    先跑通

--- src/main/java/org/whiteandpaint/rhyus/processor/BCProcessor.java
+++ src/main/java/org/whiteandpaint/rhyus/processor/BCProcessor.java
@@ -11,52 +11,58 @@ import java.util.Map;
 public class BCProcessor {
 
     public static void commandResolve(ChannelHandlerContext ctx, String command) {
-        if (command.startsWith("tell")) {
-            // 发送文本给指定用户
-            String to = command.split(" ")[1];
-            String content = command.replace("tell " + to + " ", "");
-            System.out.println("tell " + to + ": " + content);
-            for (Map.Entry<ChannelHandlerContext, JSONObject> entry : AuthProcessor.onlineUsers.entrySet()) {
-                String name = entry.getValue().optString("userName");
-                if (to.equals(name)) {
-                    sendText(entry.getKey(), content);
+        try {
+            if (command.startsWith("tell")) {
+                // 发送文本给指定用户
+                String to = command.split(" ")[1];
+                String content = command.replace("tell " + to + " ", "");
+                System.out.println("tell " + to + ": " + content);
+                for (Map.Entry<ChannelHandlerContext, JSONObject> entry : AuthProcessor.onlineUsers.entrySet()) {
+                    String name = entry.getValue().optString("userName");
+                    if (to.equals(name)) {
+                        sendText(entry.getKey(), content);
+                    }
                 }
-            }
-        } else if (command.startsWith("msg")) {
-            // 广播文本：指定发送者先收到消息
-            String sender = command.split(" ")[1];
-            String content = command.replace("msg " + sender + " ", "");
-            System.out.println("msg " + sender + ": " + content);
-            for (Map.Entry<ChannelHandlerContext, JSONObject> entry : AuthProcessor.onlineUsers.entrySet()) {
-                String name = entry.getValue().optString("userName");
-                if (sender.equals(name)) {
-                    sendText(entry.getKey(), content);
+            } else if (command.startsWith("msg")) {
+                // 广播文本：指定发送者先收到消息
+                String sender = command.split(" ")[1];
+                String content = command.replace("msg " + sender + " ", "");
+                System.out.println("msg " + sender + ": " + content);
+                for (Map.Entry<ChannelHandlerContext, JSONObject> entry : AuthProcessor.onlineUsers.entrySet()) {
+                    String name = entry.getValue().optString("userName");
+                    if (sender.equals(name)) {
+                        sendText(entry.getKey(), content);
+                    }
                 }
-            }
 
-            for (Map.Entry<ChannelHandlerContext, JSONObject> entry : AuthProcessor.onlineUsers.entrySet()) {
-                String name = entry.getValue().optString("userName");
-                if (!sender.equals(name)) {
-                    sendText(entry.getKey(), content);
+                for (Map.Entry<ChannelHandlerContext, JSONObject> entry : AuthProcessor.onlineUsers.entrySet()) {
+                    String name = entry.getValue().optString("userName");
+                    if (!sender.equals(name)) {
+                        sendText(entry.getKey(), content);
+                        Thread.sleep(10);
+                    }
                 }
+            } else if (command.startsWith("all")) {
+                // 广播文本：直接广播，所有人按顺序收到消息
+                String content = command.replace("all ", "");
+                System.out.println("all: " + content);
+                for (Map.Entry<ChannelHandlerContext, JSONObject> entry : AuthProcessor.onlineUsers.entrySet()) {
+                    sendText(entry.getKey(), content);
+                    Thread.sleep(10);
+                }
+            } else if (command.startsWith("slow")) {
+                // 广播文本：慢广播，慢速发送，但所有人都能收到
+                // 广播文本：直接广播，所有人按顺序收到消息
+                String content = command.replace("slow ", "");
+                System.out.println("slow: " + content);
+                for (Map.Entry<ChannelHandlerContext, JSONObject> entry : AuthProcessor.onlineUsers.entrySet()) {
+                    sendText(entry.getKey(), content);
+                    Thread.sleep(100);
+                }
+            } else if (command.equals("online")) {
+                sendText(ctx, Arrays.toString(AuthProcessor.onlineUsers.values().toArray()));
             }
-        } else if (command.startsWith("all")) {
-            // 广播文本：直接广播，所有人按顺序收到消息
-            String content = command.replace("all ", "");
-            System.out.println("all: " + content);
-            for (Map.Entry<ChannelHandlerContext, JSONObject> entry : AuthProcessor.onlineUsers.entrySet()) {
-                sendText(entry.getKey(), content);
-            }
-        } else if (command.startsWith("slow")) {
-            // 广播文本：慢广播，慢速发送，但所有人都能收到
-            // 广播文本：直接广播，所有人按顺序收到消息
-            String content = command.replace("slow ", "");
-            System.out.println("slow: " + content);
-            for (Map.Entry<ChannelHandlerContext, JSONObject> entry : AuthProcessor.onlineUsers.entrySet()) {
-                sendText(entry.getKey(), content);
-            }
-        } else if (command.equals("online")) {
-            sendText(ctx, Arrays.toString(AuthProcessor.onlineUsers.values().toArray()));
+        } catch (Exception ignored) {
         }
     }
 
